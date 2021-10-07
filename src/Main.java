1c70a9916267adb9db21458e18abe6d79c6990b6
erictomson <erictomson@gmail.com>
2021-10-07 20:51:58 -0300

    Inseridos comentários descritivos

--- src/Main.java
+++ src/Main.java
@@ -27,20 +27,33 @@ public class Main {
         saida += "\nSet de elementos não duplicados e ordenados:\n";
         this.mostrarSet(treeSet);
 
+        // headSet: retorna subconjunto com os elementos das
+        // posições anteriores ao elemento informado
         saida += "\n\nSubconjunto de TreeSet menor que 'laranja':\n";
         this.mostrarSet(treeSet.headSet("laranja"));
 
+        // tailSet: retorna subconjunto com os elementos das
+        // posições posteriores ao elemento informado
         saida += "\n\nSubconjunto de TreeSet maior que 'laranja':\n";
         this.mostrarSet(treeSet.tailSet("laranja"));
 
+        // Alterações nos subconjuntos gerados por headSet e tailSet
+        // alterarão o conjunto TreeSet original
+
+        // Retorna o primeiro elemento do TreeSet
         saida += "\n\nPrimeiro elemento de set: " + treeSet.first();
+        // Retorna o último elemento do TreeSet
         saida += "\nÚltimo elemento de set: " + treeSet.last();
 
+        // Configurar TextArea para receber a string
         JTextArea saidaArea = new JTextArea();
+        // Atribuindo a string à TextArea para exibição
         saidaArea.setText(saida);
 
+        // Exibindo a TextArea montada em uma caixa de diálogo
         JOptionPane.showMessageDialog(null,saidaArea,"Trabalhando com Sets",JOptionPane.INFORMATION_MESSAGE);
 
+        // Saindo do sistema
         System.exit(0);
     }
 
